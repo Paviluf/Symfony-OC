c7197b27e274287caf4ab52f00b9a359dc91f1da
Jeremy <jeremy9856@gmail.com>
2018-03-14 18:25:24 +0100

    add crud routes, templates and actions

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -6,8 +6,33 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
         return $this->render('@OCPlatform/Advert/index.html.twig');
+    }
+
+    public function viewAction($id)
+    {
+        return $this->render('@OCPlatform/Advert/view.html.twig', array('id' => $id));
+    }
+
+    public function viewSlugAction($year, $slug, $_format)
+    {
+        return $this->render('@OCPlatform/Advert/view.html.twig');
+    }
+
+    public function addAction()
+    {
+        return $this->render('@OCPlatform/Advert/edit.html.twig');
+    }
+
+    public function editAction($id)
+    {
+        return $this->render('@OCPlatform/Advert/edit.html.twig');
+    }
+
+    public function deleteAction($id)
+    {
+        return $this->render('@OCPlatform/Advert/edit.html.twig');
     }
 }
