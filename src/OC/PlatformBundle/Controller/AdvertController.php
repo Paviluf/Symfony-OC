8c5396abc84b6d5da0031a624584835131344ca0
Jeremy <jeremy9856@gmail.com>
2018-03-15 10:33:15 +0100

    start implement the actions in AdvertController

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -3,11 +3,17 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
     public function indexAction($page)
     {
+        if($page < 1) {
+            throw new NotFoundHttpException('Page '.$page.' inexistante');    
+        }
+
         return $this->render('@OCPlatform/Advert/index.html.twig');
     }
 
@@ -21,13 +27,23 @@ class AdvertController extends Controller
         return $this->render('@OCPlatform/Advert/view.html.twig');
     }
 
-    public function addAction()
+    public function addAction(Request $request)
     {
+        if($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('info', 'annonce bien enregistrée');
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+
         return $this->render('@OCPlatform/Advert/edit.html.twig');
     }
 
     public function editAction($id)
     {
+        if($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('info', 'annonce bien enregistrée');
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+        
         return $this->render('@OCPlatform/Advert/edit.html.twig');
     }
 
