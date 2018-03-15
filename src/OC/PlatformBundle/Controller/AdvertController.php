68e9b1e34ad2ff1ab91c169a039104c2a9ee5533
Jeremy <jeremy9856@gmail.com>
2018-03-15 21:45:14 +0100

    workaround Symfony limitation for default params when prefix is used

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -8,13 +8,19 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
-    public function indexAction($page)
+    public function indexAction(Request $request, $page)
     {
         if($page < 1) {
             throw new NotFoundHttpException('Page '.$page.' inexistante');    
         }
 
-        return $this->render('@OCPlatform/Advert/index.html.twig');
+        // Workaround this limitation : https://stackoverflow.com/questions/46267523/symfony-3-parameters-default-value-not-returned
+        $routeName = $request->get('_route');
+        if($routeName == 'oc_platform_home_page' && $page == 1) {
+            return $this->redirectToRoute('oc_platform_home');
+        }
+
+        return $this->render('@OCPlatform/Advert/index.html.twig', array('test' => $page));
     }
 
     public function viewAction($id)
